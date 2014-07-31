7d0e4e03185bbd2b915a134d1c1403ae20d41b07
Cathleen Gendron <gendron@mit.edu>
2014-07-31 03:54:29 -0400

    calender container

--- main.js
+++ main.js
@@ -6,16 +6,14 @@ $(function() {
     var isHalfHour = i % 2;
     var hour = Math.floor(i / 2) + 9;
     hour = hour === 12 ? 12 : hour % 12;
-    console.log(i, hour, isHalfHour)
     var isAfternoon = i > 5;
     var minutes = isHalfHour ? '30' : '00';
     var meridiem = isAfternoon ? 'PM' : 'AM';
     var content = hour.toString() + ':' + minutes + ' <span>' + meridiem + '</span>';
     var html = '<tr><td class="time">' + content + '</td></tr>';
-    // if(i === 0) {
-    //   html += '<td rowspan="25"><div id="container"></div></td>'
-    // }
     table.append(html);
-    // console.log(html)
+    if(i === 0) {
+      $('tr').append('<td rowspan="25"><div id="container"></div></td>')
+    }
   }
 })
