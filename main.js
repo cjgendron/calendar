48be09e99f47983f9dd6a44946ce7eafec7d2bcd
Cathleen Gendron <gendron@mit.edu>
2014-08-02 01:04:06 -0400

    Cols aren't working for >2 events in conflict. CSS needs some tweaking.

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 $(function() {
-  // var table = $('#main');
-  // // console.log(table);
+  var table = $('#main');
+  // console.log(table);
 
-  // for (var i = 0; i < 25; i++) {
-  //   var isHalfHour = i % 2;
-  //   var hour = Math.floor(i / 2) + 9;
-  //   hour = hour === 12 ? 12 : hour % 12;
-  //   var isAfternoon = i > 5;
-  //   var minutes = isHalfHour ? '30' : '00';
-  //   var meridiem = isAfternoon ? 'PM' : 'AM';
-  //   var content = hour.toString() + ':' + minutes + ' <span>' + meridiem + '</span>';
-  //   var html = '<tr><td class="time">' + content + '</td></tr>';
-  //   table.append(html);
-  //   if(i === 0) {
-  //     $('tr').append('<td rowspan="25"><div id="container"></div></td>');
-  //   }
-  // }
+  for (var i = 0; i < 25; i++) {
+    var isHalfHour = i % 2;
+    var hour = Math.floor(i / 2) + 9;
+    hour = hour === 12 ? 12 : hour % 12;
+    var isAfternoon = i > 5;
+    var minutes = isHalfHour ? '30' : '00';
+    var meridiem = isAfternoon ? 'PM' : 'AM';
+    var content = hour.toString() + ':' + minutes + ' <span>' + meridiem + '</span>';
+    var html = '<tr><td class="time">' + content + '</td></tr>';
+    table.append(html);
+    if(i === 0) {
+      $('tr').append('<td rowspan="25"><div id="container"></div></td>');
+    }
+  }
 })
 
 
 var layOutDay = function(events) {
+
+  $('#container').empty();
 
   events.sort(function(evt1, evt2) {
     return evt1.start - evt2.start;
@@ -32,7 +34,7 @@ var layOutDay = function(events) {
   }
 
   events.forEach(function(element, index, array) {
-    console.log(index, element);
+    // console.log(index, element);
     for(var time = element.start; time <= element.end; time++) {
       timeSlots[time].push(index);
       // if (timeSlots[time]) {
@@ -60,22 +62,53 @@ var layOutDay = function(events) {
     for (i in conflictSet) {
       var index = conflictSet[i];
       var width = events[index]['width'];
+      var cols = _.range(width);
+      // console.log(cols);
       if(width) {
+        cols = _.without(cols, events[index]['col']);
         setWidths = true;
         for(j in conflictSet) {
-          var index = conflictSet[j]
+          var index = conflictSet[j];
           events[index]['width'] = width;
+          events[index]['col'] = cols.pop();
         }
+        break;
       }
     }
     if(!setWidths) {
       for (i in conflictSet) {
         var index = conflictSet[i];
-        events[index]['width'] = 600/conflictSet.length;
+        events[index]['width'] = conflictSet.length;
+        events[index]['col'] = i;
       }
     }
     timeSlots.splice(maxLengthIndex, 1);
-    console.log(JSON.stringify(events));
+    // console.log(JSON.stringify(events));
   } 
 
+  HTMLify(events);
+
 }
+
+
+var HTMLify = function(events) {
+  for(var i = 0; i < events.length; i++) {
+    var evt = events[i];
+    $('<div></div>', {
+      'class': 'event',
+      'css': {
+        'top': evt['start'] + 'px',
+        'left': (evt['col'] * 600/evt['width'] + 10).toString() + 'px',
+        'width': (600/evt['width']).toString() + 'px',
+        'height': (evt['end'] - evt['start']).toString() + 'px',
+      },
+      'html': '<span class="item">Sample Item</span><br><span class="loc">Sample Location</span>'
+    }).appendTo('#container');
+  }
+}
+
+
+
+
+
+
