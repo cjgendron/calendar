a7410c0bd2202fb4175ae5e6b4069afc8d607105
Cathleen Gendron <gendron@mit.edu>
2014-08-02 21:09:57 -0400

    cols are fixed (for now), edge case gives wrong width assignment

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function() {
   var table = $('#main');
-  // console.log(table);
 
+  // format times
   for (var i = 0; i < 25; i++) {
     var isHalfHour = i % 2;
     var hour = Math.floor(i / 2) + 9;
@@ -18,30 +18,41 @@ $(function() {
   }
 })
 
+  // ALGORITHM OVERVIEW:
+  // 1) find all sets of events which conflict with each other
+  // 2) start with the largest set of 
 
 var layOutDay = function(events) {
 
   $('#container').empty();
 
+
+  // sort all events by start time
   events.sort(function(evt1, evt2) {
     return evt1.start - evt2.start;
   })
 
-  // use so we can do Math.max.apply later
+  // 
   var timeSlots = [];
   for(var i = 0; i <= 720; i++) {
     timeSlots.push([]);
   }
-
   events.forEach(function(element, index, array) {
     for(var time = element.start; time <= element.end; time++) {
       timeSlots[time].push(index);
     }
   })
 
+
   var timeSlotsStringified = timeSlots.map(function(arr) { return JSON.stringify(arr); });
   var filteredTimeSlots = _.uniq(timeSlotsStringified);
   timeSlots = filteredTimeSlots.map(function(str) { return JSON.parse(str); });
+
+  // BUG: say we have these sets of conflicts:
+  // [0,1,2,3,4]
+  // [4,5]
+  // [5,6,7]
+  //We'll sign width 3 to event 5, when it should be getting width 5 from 4.
 
   while(timeSlots.length) {
     // find the largest set of conflicting events
@@ -50,14 +61,17 @@ var layOutDay = function(events) {
     var conflictSet = timeSlots[maxLengthIndex];
     var setWidths = false;
     // TODO: clean this up!
-    for (i in conflictSet) {
+    for (var i = 0; i < conflictSet.length; i++) {
       var index = conflictSet[i];
+      var evt = events[index];
       var width = events[index]['width'];
+      // console.log("width: " + width);
       var cols = _.range(width);
       if(width) {
-        cols = _.without(cols, events[index]['col']);
+        var occupiedCols = _.pluck(conflictSet.map(function(i) { return events[i]; } ), 'col');
+        cols = _.difference(cols, occupiedCols);
         setWidths = true;
-        for(j in conflictSet) {
+        for(var j = 0; j < conflictSet.length; j++) {
           var index = conflictSet[j];
           if(!events[index]['width']) {
             events[index]['width'] = width;
@@ -68,7 +82,7 @@ var layOutDay = function(events) {
       }
     }
     if(!setWidths) {
-      for (i in conflictSet) {
+      for (var i = 0; i < conflictSet.length; i++) {
         var index = conflictSet[i];
         events[index]['width'] = conflictSet.length;
         events[index]['col'] = Number(i);
@@ -98,6 +112,24 @@ var HTMLify = function(events) {
   }
 }
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var generate = function(quantity) {
+  var events = [];
+  for(var i = 0; i < quantity; i++) {
+    var start = getRandomInt(0, 700);
+    var end = getRandomInt(start+10, Math.min(start+100, 720));
+    events.push({start: start, end: end});
+  }
+
+  events.sort(function(evt1, evt2) {
+  return evt1.start - evt2.start;
+  })
+
+  return events;
+}
 
 
 
